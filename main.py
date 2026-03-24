e502c33959c13255d8a0502e3827f1ff3d440846
Egem97 <pierenriquez5@gmail.com>
2026-03-24 07:26:43 -0500

    fixed cronm

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ async def main():
     scheduler = AsyncIOScheduler(timezone=pytz.timezone('America/Lima'))
     
     # Ejecutar cada 30 minutos desde las 8:00 AM hasta las 22:30 PM (10:30 PM)
-    #scheduler.add_job(pipeline_data_c1, 'cron', hour='8-22', minute='0,30')
-    scheduler.add_job(pipeline_data_c1, 'cron', hour='22', minute='45')
+    scheduler.add_job(pipeline_data_c1, 'cron', hour='7-22', minute='0,30')
+    #scheduler.add_job(pipeline_data_c1, 'cron', hour='22', minute='45')
     
     scheduler.start()
     print("Scheduler iniciado. Ejecutando jobs.")
